5edc52f2b3d120d1933cc390e0b16dd3f3a9281c
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-10 10:24:45 +0100

    Backing up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Card from './component/Card';
+import data from './data';
 
 function App() {
+    let eachTour = data.map(eachTourData => {
+       return (
+        <Card 
+        {...eachTourData}
+    />
+       )
+        
+    })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+        <Header />
+        {/* <Card 
+            imageUrl="https://source.unsplash.com/WLxQvbMyfas"
+            location="Japan"
+            title= "Mount Fuji"
+            startDate= "12 Jan, 2021"
+            endDate= "24 Jan, 2021"
+            description= "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+
+        /> */}
+        {eachTour}
+        <Footer />
+	</div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "As-Salaam Masjid",
+        location: "Camp, Funaab",
+        googleMapsUrl: "https://goo.gl/maps/iikWS751T2kwt2uy6",
+        startDate: "01 April, 2022",
+        endDate: "10 April, 2022",
+        description: "As-Salaam Masjid do organize daily Iftaar during Ramadan and Tafseer during weekdays (after Solaatul 'Asr).",
+        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipP7JcW2FgH5-9dVoupVw3-w7BFhDXNnItn7AB71=w408-h306-k-no"
+    },
+    {
+        title: "Al-Adheem Masjid",
+        location: "Camp, Funaab",
+        googleMapsUrl: "https://goo.gl/maps/eBKapsWcK5BUzm3w7",
+        startDate: "01 April, 2022",
+        endDate: "10 April, 2022",
+        description: "Al-Adheem Masjid do organize Iftaar and Tafseer on Weekends during Ramadan (after Solaatul 'Asr).",
+        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipOS5Z9QNJQd4DgUKcjXHIZ5z-zDU7uIIrfDTvkh=w408-h408-k-no"
+    },
+    {
+        title: "Masjid Sunnah",
+        location: "Camp, Funaab",
+        googleMapsUrl: "https://goo.gl/maps/5KAxV2VNWvQV6Dff6",
+        startDate: "01 April, 2022",
+        endDate: "10 April, 2022",
+        description: "Sunnah Masjid is led by our Ustaadh Engr Olonade, Aboo Ameenah. Jumu'ah is been observed there every Friday",
+        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipNW37h3UbFb5F5-4OvIAgYWQx_1E28lLShy-qf-=w408-h408-k-no"
+    }
+]
